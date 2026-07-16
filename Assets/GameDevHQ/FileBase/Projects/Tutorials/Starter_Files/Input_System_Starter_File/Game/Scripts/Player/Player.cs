9adab557e31e942a2fe9ce32920a37f5a57fd62f
jkmcauley <jkmc1964@icloud.com>
2026-07-16 13:26:15 -0500

    Initial commit

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Input_System_Starter_File/Game/Scripts/Player/Player.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Scripts.LiveObjects;
+using Cinemachine;
+using UnityEngine.InputSystem;
+
+namespace Game.Scripts.Player
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class Player : MonoBehaviour
+    {
+        private CharacterController _controller;
+        private Animator _anim;
+        [SerializeField]
+        private float _speed = 5.0f;
+        private bool _playerGrounded;
+        [SerializeField]
+        private Detonator _detonator;
+        private bool _canMove = true;
+        [SerializeField]
+        private CinemachineVirtualCamera _followCam;
+        [SerializeField]
+        private GameObject _model;
+        private InputSystem_Actions _input;
+
+
+        private void OnEnable()
+        {
+            InteractableZone.onZoneInteractionComplete += InteractableZone_onZoneInteractionComplete;
+            Laptop.onHackComplete += ReleasePlayerControl;
+            Laptop.onHackEnded += ReturnPlayerControl;
+            Forklift.onDriveModeEntered += ReleasePlayerControl;
+            Forklift.onDriveModeExited += ReturnPlayerControl;
+            Forklift.onDriveModeEntered += HidePlayer;
+            Drone.OnEnterFlightMode += ReleasePlayerControl;
+            Drone.onExitFlightmode += ReturnPlayerControl;
+            _input.Player.Enable();
+        }
+
+        private void Awake()
+        {
+            _input = new InputSystem_Actions();
+
+        }
+
+
+
+
+        private void Start()
+
+        {
+            _controller = GetComponent<CharacterController>();
+
+            if (_controller == null)
+                Debug.LogError("No Character Controller Present");
+
+            _anim = GetComponentInChildren<Animator>();
+
+            if (_anim == null)
+                Debug.Log("Failed to connect the Animator");
+
+        }
+
+        private void Update()
+        {
+            if (_canMove == true)
+                CalcutateMovement();
+
+        }
+
+        private void CalcutateMovement()
+        {
+            _playerGrounded = _controller.isGrounded;
+            // float h = Input.GetAxisRaw("Horizontal");
+            // float v = Input.GetAxisRaw("Vertical");
+            Vector2 inputValue = _input.Player.Move.ReadValue<Vector2>();
+            float h = inputValue.x;
+            float v = inputValue.y;
+            transform.Rotate(transform.up, h);
+
+            var direction = transform.forward * v;
+            var velocity = direction * _speed;
+
+
+            _anim.SetFloat("Speed", Mathf.Abs(velocity.magnitude));
+
+
+            if (_playerGrounded)
+                velocity.y = 0f;
+            if (!_playerGrounded)
+            {
+                velocity.y += -20f * Time.deltaTime;
+            }
+
+            _controller.Move(velocity * Time.deltaTime);
+
+        }
+
+        private void InteractableZone_onZoneInteractionComplete(InteractableZone zone)
+        {
+            switch (zone.GetZoneID())
+            {
+                case 1: //place c4
+                    _detonator.Show();
+                    break;
+                case 2: //Trigger Explosion
+                    TriggerExplosive();
+                    break;
+            }
+        }
+
+        private void ReleasePlayerControl()
+        {
+            _canMove = false;
+            _followCam.Priority = 9;
+        }
+
+        private void ReturnPlayerControl()
+        {
+            _model.SetActive(true);
+            _canMove = true;
+            _followCam.Priority = 10;
+        }
+
+        private void HidePlayer()
+        {
+            _model.SetActive(false);
+        }
+
+        private void TriggerExplosive()
+        {
+            _detonator.TriggerExplosion();
+        }
+
+        private void OnDisable()
+        {
+            InteractableZone.onZoneInteractionComplete -= InteractableZone_onZoneInteractionComplete;
+            Laptop.onHackComplete -= ReleasePlayerControl;
+            Laptop.onHackEnded -= ReturnPlayerControl;
+            Forklift.onDriveModeEntered -= ReleasePlayerControl;
+            Forklift.onDriveModeExited -= ReturnPlayerControl;
+            Forklift.onDriveModeEntered -= HidePlayer;
+            Drone.OnEnterFlightMode -= ReleasePlayerControl;
+            Drone.onExitFlightmode -= ReturnPlayerControl;
+            _input.Player.Disable();
+        }
+
+    }
+}
+
+
